b23d942ef6c1533f3c887121ce55986feb36c600
Ronny2101 <dilshoddaminov2121@gmail.com>
2026-06-09 17:56:24 +0900

    feat: Initial input

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   /* config options here */
 };
 
